0330f2af4e7a1e39b7081c875b59d468dd1bf9b2
Javier Flores <javier.floresapodaca@gmail.com>
2020-08-24 15:34:45 -0700

    Sprint Complete - All tests passed.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,26 @@
 var HoneyMakerBee = function() {
+  Bee.call(this) {
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+
+  }
 };
 
+// HoneyMakerBee.prototype {
+
+// }
+
+HoneyMakerBee.prototype.makeHoney = function() {this.honeyPot = this.honeyPot++};
+
+HoneyMakerBee.prototype.giveHoney = function() {this.honeyPot = this.honeyPot--;};
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+// HoneyMakerBee.prototype = Object.create(Grub.prototype);
+
+// HoneyMakerBee.prototype.makeHoney = function() {this.honey = this.honey++};
+
+// HoneyMakerBee.prototype.makeHoney = function() {this.honey = this.honey - 1;};
